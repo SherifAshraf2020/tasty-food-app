aa41b7279f0de4f683cb1af5d68f71e564962d66
Sherif Ashraf <sherif.ashraf.farooq@gmail.com>
2026-02-02 22:21:16 +0200

    added shared Preference as datasource and add splash presenter and its Implementation

---
+++ app/src/main/java/com/example/tasty_food_app/SplashPresenter.java
@@ -0,0 +1,5 @@
+package com.example.tasty_food_app;
+
+public interface SplashPresenter {
+
+}
---
+++ app/src/main/java/com/example/tasty_food_app/datasource/SharedPrefsLocalDataSource.java
@@ -0,0 +1,25 @@
+package com.example.tasty_food_app.datasource;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SharedPrefsLocalDataSource {
+    private static final String PREF_NAME = "tasty-food-prefs";
+    private static final String KEY_ONBOARDING = "is_onboarding_finished";
+    private final SharedPreferences sharedPreferences;
+
+    public SharedPrefsLocalDataSource(Context context) {
+        sharedPreferences =
+                context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+    }
+
+    public void setOnBoardingFinished(boolean isFinished){
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("onboarding_finished", isFinished);
+        editor.apply();
+    }
+
+    public Boolean isOnBoardingFinished(){
+        return sharedPreferences.getBoolean(KEY_ONBOARDING, false);
+    }
+}
