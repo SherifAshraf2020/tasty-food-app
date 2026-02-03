44157d71491e011366daf6a6ade7143e9ec90da9
Sherif Ashraf <sherif.ashraf.farooq@gmail.com>
2026-02-03 20:57:43 +0200

    added third fragment forget password

---
+++ app/src/main/java/com/example/tasty_food_app/auth/ForgetPasswordFragment.java
@@ -0,0 +1,24 @@
+package com.example.tasty_food_app.auth;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.tasty_food_app.R;
+
+
+public class ForgetPasswordFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_forget_password, container, false);
+    }
+}
