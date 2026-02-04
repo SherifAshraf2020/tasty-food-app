38e5f83c19626c82e18df668a8a9fa12c79e8142
Sherif Ashraf <sherif.ashraf.farooq@gmail.com>
2026-02-04 20:57:19 +0200

    added forget password Presenter interface and its imp

---
+++ app/src/main/java/com/example/tasty_food_app/auth/ForgetPasswordPresenter.java
@@ -0,0 +1,5 @@
+package com.example.tasty_food_app.auth;
+
+public interface ForgetPasswordPresenter {
+    void sendResetEmail(String email);
+}
---
+++ app/src/main/java/com/example/tasty_food_app/auth/ForgetPasswordPresenterImp.java
@@ -0,0 +1,33 @@
+package com.example.tasty_food_app.auth;
+
+import com.example.tasty_food_app.datasource.remote.AuthNetworkResponse;
+import com.example.tasty_food_app.datasource.repository.AuthRepository;
+
+public class ForgetPasswordPresenterImp implements ForgetPasswordPresenter{
+    private ForgetPasswordView forgetPasswordView;
+    private AuthRepository authRepository;
+
+    public  ForgetPasswordPresenterImp(ForgetPasswordView forgetPasswordView, AuthRepository authRepository){
+        this.forgetPasswordView = forgetPasswordView;
+        this.authRepository = authRepository;
+    }
+    @Override
+    public void sendResetEmail(String email) {
+        if (forgetPasswordView != null) {
+            forgetPasswordView.showLoading();
+        }
+        authRepository.ResetPassword(email, new AuthNetworkResponse() {
+            @Override
+            public void onSuccess() {
+                forgetPasswordView.hideLoading();
+                forgetPasswordView.onEmailSentSuccess();
+            }
+
+            @Override
+            public void onFailure(String error) {
+                forgetPasswordView.hideLoading();
+                forgetPasswordView.onEmailSentError(error);
+            }
+        });
+    }
+}
