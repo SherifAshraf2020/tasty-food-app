770de818400420c73e7425741ddc222dc45b6884
Sherif Ashraf <sherif.ashraf.farooq@gmail.com>
2026-02-04 10:34:00 +0200

    add SignUpPresenter and its implementation then SignUpView

---
+++ app/src/main/java/com/example/tasty_food_app/auth/SignUpPresenter.java
@@ -0,0 +1,5 @@
+package com.example.tasty_food_app.auth;
+
+public interface SignUpPresenter {
+    void signUp(String name, String email, String password);
+}
---
+++ app/src/main/java/com/example/tasty_food_app/auth/SignUpPresenterImp.java
@@ -0,0 +1,10 @@
+package com.example.tasty_food_app.auth;
+
+public class SignUpPresenterImp implements SignUpPresenter {
+
+
+    @Override
+    public void signUp(String name, String email, String password) {
+
+    }
+}
