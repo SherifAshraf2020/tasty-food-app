4c6a093368f471b7952d10d5c6c05b0fd4e2a499
Sherif Ashraf <sherif.ashraf.farooq@gmail.com>
2026-02-04 10:46:06 +0200

    Implement EmailAuthService

--- app/src/main/java/com/example/tasty_food_app/datasource/remote/EmailAuthService.java
+++ app/src/main/java/com/example/tasty_food_app/datasource/remote/EmailAuthService.java
@@ -1,4 +1,25 @@
 package com.example.tasty_food_app.datasource.remote;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class EmailAuthService {
+
+    private FirebaseAuth mAuth;
+
+    public EmailAuthService() {
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void signUp(String email, String password, AuthNetworkResponse callback) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        callback.onSuccess();
+                    } else {
+                        String error = (task.getException() != null) ?
+                                task.getException().getMessage() : "Auth Failed";
+                        callback.onFailure(error);
+                    }
+                });
+    }
 }
