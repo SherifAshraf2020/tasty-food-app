220092de6ebb25ddf23a6973b9de5779ebc10be5
Sherif Ashraf <sherif.ashraf.farooq@gmail.com>
2026-02-05 20:44:16 +0200

    added Bottom nav bar to home meal plan and settings

--- app/src/main/java/com/example/tasty_food_app/MainActivity.java
+++ app/src/main/java/com/example/tasty_food_app/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.tasty_food_app;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +9,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.splashscreen.SplashScreen;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     private boolean isAppReady = false;
+
+    private BottomNavigationView bottomNav;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         splashScreen.setKeepOnScreenCondition(() -> !isAppReady);
         setContentView(R.layout.activity_main);
+
+        bottomNav = findViewById(R.id.bottomNavigationView);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.fragmentContainerView);
+
+        if (navHostFragment != null) {
+            navController = navHostFragment.getNavController();
+            NavigationUI.setupWithNavController(bottomNav, navController);
+
+            navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+                int id = destination.getId();
+                if (id == R.id.homeFragment || id == R.id.mealPlanFragment || id == R.id.settingsFragment) {
+                    bottomNav.setVisibility(View.VISIBLE);
+                } else {
+                    bottomNav.setVisibility(View.GONE);
+                }
+            });
+        }
     }
 
     public void onAppReady() {
