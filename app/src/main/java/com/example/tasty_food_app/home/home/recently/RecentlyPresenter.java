aac0e31f7d8c44b7d67f02264c8b8f5411a987f0
Sherif Ashraf <sherif.ashraf.farooq@gmail.com>
2026-02-07 21:52:53 +0200

    add get from recently

--- app/src/main/java/com/example/tasty_food_app/home/home/recently/RecentlyPresenter.java
+++ app/src/main/java/com/example/tasty_food_app/home/home/recently/RecentlyPresenter.java
@@ -1,4 +1,6 @@
 package com.example.tasty_food_app.home.home.recently;
 
 public interface RecentlyPresenter {
+    void getRecentMeals();
+    void clearResources();
 }
--- app/src/main/java/com/example/tasty_food_app/home/home/recently/RecentlyPresenterImp.java
+++ app/src/main/java/com/example/tasty_food_app/home/home/recently/RecentlyPresenterImp.java
@@ -1,4 +1,45 @@
 package com.example.tasty_food_app.home.home.recently;
 
-public class RecentlyPresenterImp {
+import com.example.tasty_food_app.datasource.repository.MealRepository;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class RecentlyPresenterImp implements RecentlyPresenter{
+    private RecentlyView recentlyView;
+    private MealRepository mealRepository;
+    private final CompositeDisposable disposables = new CompositeDisposable();
+
+    public RecentlyPresenterImp(RecentlyView recentlyView, MealRepository mealRepository) {
+        this.recentlyView = recentlyView;
+        this.mealRepository = mealRepository;
+    }
+
+    @Override
+    public void getRecentMeals() {
+        disposables.add(
+                mealRepository.getRecentlyViewedMeals() // الميثود اللي بترجع Observable<List<RecentMeal>>
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(
+                                recentMeals -> {
+                                    if (recentMeals != null && !recentMeals.isEmpty()) {
+                                        recentlyView.showRecentMeals(recentMeals);
+                                    } else {
+                                        recentlyView.showEmptyMessage();
+                                    }
+                                },
+                                throwable -> recentlyView.showError(throwable.getMessage())
+                        )
+        );
+    }
+
+
+
+    @Override
+    public void clearResources() {
+        disposables.clear();
+        recentlyView = null; // تجنباً للـ Memory Leak
+    }
 }
