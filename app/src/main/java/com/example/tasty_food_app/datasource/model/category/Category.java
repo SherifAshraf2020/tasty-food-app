7bf81fe96fa6b869d0b6afd144596e40f8c2cbbb
Sherif Ashraf <sherif.ashraf.farooq@gmail.com>
2026-02-08 17:03:36 +0200

    added category and category response  POJO

---
+++ app/src/main/java/com/example/tasty_food_app/datasource/model/category/Category.java
@@ -0,0 +1,24 @@
+package com.example.tasty_food_app.datasource.model.category;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Category {
+    @SerializedName("idCategory")
+    private String idCategory;
+
+    @SerializedName("strCategory")
+    private String strCategory;
+
+    @SerializedName("strCategoryThumb")
+    private String strCategoryThumb;
+
+    @SerializedName("strCategoryDescription")
+    private String strCategoryDescription;
+
+    public Category() {}
+
+    // Getters
+    public String getStrCategory() { return strCategory; }
+    public String getStrCategoryThumb() { return strCategoryThumb; }
+    public String getStrCategoryDescription() { return strCategoryDescription; }
+}
