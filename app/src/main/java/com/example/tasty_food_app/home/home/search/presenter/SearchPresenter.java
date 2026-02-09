c65404c1b9e70c6cdcc6a1f6408664c513db2df1
Sherif Ashraf <sherif.ashraf.farooq@gmail.com>
2026-02-09 23:43:47 +0200

    added presneter and view interfaces

---
+++ app/src/main/java/com/example/tasty_food_app/home/home/search/presenter/SearchPresenter.java
@@ -0,0 +1,29 @@
+package com.example.tasty_food_app.home.home.search.presenter;
+
+import com.example.tasty_food_app.datasource.model.Meal;
+
+import java.util.List;
+
+public interface SearchPresenter {
+
+    void addToFavorite(Meal meal);
+    void removeFromFavorite(Meal meal);
+    void checkFavoritesAndShow(List<Meal> apiMeals);
+
+    void getCategories();
+    void getIngredients();
+    void getAreas();
+
+
+
+    void getMealsByCategory(String categoryName);
+    void getMealsByArea(String areaName);
+    void getMealsByIngredient(String ingredientName);
+
+
+    void getMealsByLetter(String letter);
+    void searchMeals(String query);
+
+
+    void clearResources();
+}
---
+++ app/src/main/java/com/example/tasty_food_app/home/home/search/view/SearchView.java
@@ -0,0 +1,26 @@
+package com.example.tasty_food_app.home.home.search.view;
+
+import com.example.tasty_food_app.datasource.model.Meal;
+import com.example.tasty_food_app.datasource.model.area.Area;
+import com.example.tasty_food_app.datasource.model.category.Category;
+import com.example.tasty_food_app.datasource.model.ingredient.Ingredient;
+
+import java.util.List;
+
+public interface SearchView {
+    void showCategories(List<Category> categories);
+    void showIngredients(List<Ingredient> ingredients);
+    void showAreas(List<Area> areas);
+
+
+
+    void showMealsResult(List<Meal> meals);
+
+
+    void showError(String message);
+    void showLoading();
+    void hideLoading();
+
+
+    void showEmptyResult();
+}
