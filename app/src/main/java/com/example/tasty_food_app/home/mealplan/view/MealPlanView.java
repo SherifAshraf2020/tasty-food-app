01004a5c63c6370a0399272b5aa768800eb67fae
Sherif Ashraf <sherif.ashraf.farooq@gmail.com>
2026-02-10 23:43:08 +0200

    added meal plan view

---
+++ app/src/main/java/com/example/tasty_food_app/home/mealplan/view/MealPlanView.java
@@ -0,0 +1,20 @@
+package com.example.tasty_food_app.home.mealplan.view;
+
+import com.example.tasty_food_app.datasource.model.PlanMeal;
+
+import java.util.List;
+
+public interface MealPlanView {
+
+    void showDaysList(List<String> days);
+
+    void showPlanData(List<PlanMeal> planMeals);
+
+    void onPlanGenerated();
+
+    void onInsertSuccess();
+
+    void onDeleteSuccess();
+
+    void onError(String message);
+}
